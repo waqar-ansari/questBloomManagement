6d15d20eb0bb27f2cd0019a9386be23d515e22f2
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-06-04 11:04:33 +0530

    some changes in pages

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,7 +1,7 @@
 export const mapLocations = [
   {
-    src: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d135905.11693909427!2d-73.95165795400088!3d41.17584829642291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1727094281524!5m2!1sen!2s",
-    location: "Office 1004, 10th Floor, Bayswater, Business Bay, Dubai-UAE, Reg.Add: A3, 3rd Floor, Dubai South Freezone, P.O Box 113225, Dubai-UAE",
+    src: "<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28880.459585507484!2d55.24949462000004!3d25.20128507330067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f42813dbc8057%3A0x3c7ace4f38f16ac0!2sServcorp%20Boulevard%20Plaza%20-%20Coworking%2C%20Offices%2C%20Virtual%20Offices%20%26%20Meeting%20Rooms!5e0!3m2!1sen!2sin!4v1749015049362!5m2!1sen!2sin"",
+      location: "Office 1004, 10th Floor, Bayswater, Business Bay, Dubai-UAE, Reg.Add: A3, 3rd Floor, Dubai South Freezone, P.O Box 113225, Dubai-UAE",
     phone: "(+971) 564 065 672",
     email: "info@questmanagement.ae",
   },
